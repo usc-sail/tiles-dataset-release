882cc1b64335f8946d6052c2540b7e2e94f95825
Karel Mundnich <mundnich@usc.edu>
2020-02-18 16:27:38 -0800

    Changes to bins in paper histograms

--- paper/figures/usage_histograms.py
+++ paper/figures/usage_histograms.py
@@ -24,21 +24,23 @@ fitbit = pd.read_csv("../data/tikz/fitbit.csv")
 omsignal = pd.read_csv("../data/tikz/omsignal.csv")
 owlinone = pd.read_csv("../data/tikz/owlinone.csv")
 
+bins = [i for i in range(0, 25)]
+
 fig, ax = plt.subplots(3,1)
 
-ax[0].hist(fitbit['hours'], label='Fitbit', color='tab:blue')
+ax[0].hist(fitbit['hours'], label='Fitbit', color='tab:blue', bins=bins)
 ax[0].set_xlim((0,24))
 ax[0].set_ylabel('Participants')
 ax[0].grid(which='major', axis='both')
 ax[0].legend()
 
-ax[1].hist(omsignal['hours'], label='OMsignal garments', color='tab:orange')
+ax[1].hist(omsignal['hours'], label='OMsignal garments', color='tab:orange', bins=bins)
 ax[1].set_xlim((0,24))
 ax[1].set_ylabel('Participants')
 ax[1].grid(which='major', axis='both')
 ax[1].legend(loc='upper left')
 
-ax[2].hist(owlinone['hours'], label="Jelly phones", color='tab:green')
+ax[2].hist(owlinone['hours'], label="Jelly phones", color='tab:green', bins=bins)
 ax[2].set_xlim((0,24))
 ax[2].set_xlabel('Average number of sensor usage hours')
 ax[2].set_ylabel('Participants')
